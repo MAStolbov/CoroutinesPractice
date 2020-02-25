13c83c5ae4c4d8bbe9304257052cf76b39d8e200
MAStolbov <stolbov1108@yandex.ru>
2020-02-25 17:58:32 +0300

    New: Changed number at background threat(with pause) then call UI threat and add number to current text at textView add PlainTextView

--- app/src/main/java/com/example/android/coroutinespractice/MainActivity.kt
+++ app/src/main/java/com/example/android/coroutinespractice/MainActivity.kt
@@ -2,11 +2,66 @@ package com.example.android.coroutinespractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.android.coroutinespractice.databinding.ActivityMainBinding
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.ButtonUpdateNumber.setOnClickListener { view: View -> changeNumber() }
+
+        binding.ButtonEnterLetter.setOnClickListener { view:View -> addText() }
+    }
+
+    private val BACKGROUND = Executors.newFixedThreadPool(2)
+
+    @Volatile
+    var number: Int = 0
+
+    var newText: String = ""
+
+
+    private fun changeNumber() {
+        BACKGROUND.submit {
+            Thread.sleep(returnRandomNumber())
+            number++
+            runOnUiThread { updateText(number) }
+        }
+    }
+
+    private fun returnRandomNumber():Long{
+        return (1_000..10_000).random().toLong()
+    }
+
+    private fun updateText(number: Int) {
+        synchronized(this) {
+            newText = binding.UpdatableText.text.toString() + ",$number "
+            binding.UpdatableText.text = newText
+        }
+    }
+
+    private fun addText(){
+        binding.UpdatableText.text = binding.UpdatableText.text.toString() + binding.inputLine.text.toString()
+    }
+
+    private fun updateScreenText() {
+        doAsync {
+            BACKGROUND.submit {
+                Thread.sleep(1_000)
+                number++
+                uiThread { updateText(number) }
+            }
+        }
+
+
     }
 }
